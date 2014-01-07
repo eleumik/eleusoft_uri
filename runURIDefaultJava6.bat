158ee6d5be9f6de91640955383f202cc350aec84
Michele Vivoda <michelevivoda@hotmail.com>
2014-01-07 09:34:47 +0100

    removed old test reports

--- runURIDefaultJava6.bat
+++
@@ -1,9 +0,0 @@
-REM HERE WITH DEFAULT PROVIDER see runURIJ4 and runURIApache
-
-call java -version
-pause
-java -classpath F:\JUnit\junit3.8.1\junit.jar;.\bin;.\lib\apacheutils.jar;.; junit.swingui.TestRunner junit.org.eleusoft.uri.URITestSuite
-call c:\java5.bat
-call java -version
-pause
-java -classpath F:\JUnit\junit3.8.1\junit.jar;.\bin;.\lib\apacheutils.jar;.; junit.swingui.TestRunner junit.org.eleusoft.uri.URITestSuite
