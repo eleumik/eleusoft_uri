02fafa0115481e3745a1443f9a2b7960c24ac591
Michele Vivoda <michelevivoda@hotmail.com>
2014-01-08 09:53:15 +0100

    remove .bat

--- runURI.bat
+++
@@ -1,4 +0,0 @@
-REM HERE WITH DEFAULT PROVIDER 
-rem see runURIJ4 and runURIApache
-
-call runURI_common
--- runURI_common.bat
+++
@@ -1,1 +0,0 @@
-java -classpath .\lib\junit.jar;.\lib\eleusoft_uri.jar;.\lib\eleusoft_uri_tests.jar; junit.swingui.TestRunner junit.org.eleusoft.uri.URITestSuite
